e61c0f5901e339ff79ae6b0e921cfdd268b323f3
OmarVelasco10 <ovelasco@ucol.mx>
2023-10-18 14:34:22 -0600

    destructuring in cardDetails was avoided to have fewer lines of code

--- src/components/CardDetails/index.tsx
+++ src/components/CardDetails/index.tsx
@@ -23,52 +23,37 @@ const Component = ({ pokemon }: CardDetailsProps) => {
     return <div>Error to fetch pokemon.</div>;
   }
 
-  const {
-    name,
-    types,
-    abilities,
-    hp,
-    attack,
-    defense,
-    specialAttack,
-    specialDefense,
-    speed,
-    weight,
-    height,
-    number,
-  } = pokemon;
-
   return (
     <CardContainer>
-      <Name> {name.toUpperCase()}</Name>
+      <Name> {pokemon.name.toUpperCase()}</Name>
       <TypeContainer>
         <Title>Type: </Title>
-        {types &&
-          types.map((item, index) => (
+        {pokemon.types &&
+          pokemon.types.map((item, index) => (
             <li key={`${item.type.name}-${index}`}>
               <Span>{item.type.name}</Span>
             </li>
           ))}
       </TypeContainer>
       <InfoContainer>
-        <InfoItem title="Number" value={number} />
-        <InfoItem title="Height" value={height} />
-        <InfoItem title="Weight" value={weight} />
+        <InfoItem title="Number" value={pokemon.number} />
+        <InfoItem title="Height" value={pokemon.height} />
+        <InfoItem title="Weight" value={pokemon.weight} />
       </InfoContainer>
       <PowerContainer>
         <StatsContainer>
           <Title>Stats</Title>
-          <Stat title="Hp" value={hp} />
-          <Stat title="Attack" value={attack} />
-          <Stat title="Defense" value={defense} />
-          <Stat title="Special Attack" value={specialAttack} />
-          <Stat title="Special Defense" value={specialDefense} />
-          <Stat title="Speed" value={speed} />
+          <Stat title="Hp" value={pokemon.hp} />
+          <Stat title="Attack" value={pokemon.attack} />
+          <Stat title="Defense" value={pokemon.defense} />
+          <Stat title="Special Attack" value={pokemon.specialAttack} />
+          <Stat title="Special Defense" value={pokemon.specialDefense} />
+          <Stat title="Speed" value={pokemon.speed} />
         </StatsContainer>
         <AbilitiesContainer>
           <Title>Abilities </Title>
-          {abilities &&
-            abilities.map((item, index) => (
+          {pokemon.abilities &&
+            pokemon.abilities.map((item, index) => (
               <li key={`${item.ability.name}-${index}`}>
                 <Span>{item.ability.name}</Span>
               </li>
